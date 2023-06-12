ab8bf95178e3781458a1f82e189698ee25854aa5
Andrew Trotman <andrew.trotman@otago.ac.nz>
2023-06-12 20:57:02 +1200

    harley_seal is slower

--- at_hammingDist.cpp
+++ at_hammingDist.cpp
@@ -152,7 +152,17 @@ forceinline __m512i popcnt_AVX512BW_lookup_original(__m512i vec)
 	return ret;
 	}
 
+__m512i hs512_popcount(const __m512i v)
+{
+  const __m512i m1 = _mm512_set1_epi8(0x55);
+  const __m512i m2 = _mm512_set1_epi8(0x33);
+  const __m512i m4 = _mm512_set1_epi8(0x0F);
 
+  const __m512i t1 = _mm512_sub_epi8(v,       (_mm512_srli_epi16(v,  1) & m1));
+  const __m512i t2 = _mm512_add_epi8(t1 & m2, (_mm512_srli_epi16(t1, 2) & m2));
+  const __m512i t3 = _mm512_add_epi8(t2, _mm512_srli_epi16(t2, 4)) & m4;
+  return _mm512_sad_epu8(t3, _mm512_setzero_si512());
+}
 
 /*
 forceinline __m256i hs_popcount(const __m256i v)
@@ -230,6 +240,7 @@ size_t at512_filterAndSortByHammingDistance(uint64_t a, const std::vector<uint64
 		__m512i data = _mm512_loadu_si512((__m512i_u *)current);
 		__m512i xorResult = _mm512_xor_epi64(key, data);
 		__m512i counts = popcnt_AVX512BW_lookup_original(xorResult);
+//		__m512i counts = hs512_popcount(xorResult);
 
 		__mmask8 triggers = _mm512_cmple_epi64_mask(counts, threshold);
 		_mm512_mask_compressstoreu_epi64(current_answer, triggers, data);
