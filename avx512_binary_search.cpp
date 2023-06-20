c3dcb4594fef605663b3a554900f709337b0c2d7
Andrew Trotman <andrew@cs.otago.ac.nz>
2023-06-20 16:49:33 +1200

    8-way concurrent AVX512 binary search

--- avx512_binary_search.cpp
+++ avx512_binary_search.cpp
@@ -2,8 +2,9 @@
 #include <immintrin.h>
 
 #include <bitset>
-#include <iostream>
+#include <chrono>
 #include <random>
+#include <iostream>
 
 /*
 	AVX_BINARY_SEARCH()
@@ -11,17 +12,17 @@
 */
 __m512i avx_binary_search(uint64_t *array, int64_t elements, __m512i key)
 	{
-	__mmask8 finished = 0;
+	__mmask8 not_finished = 0;
 	__m512i one = _mm512_set1_epi64(1);
 	__m512i lower = _mm512_set1_epi64(-1);
 	__m512i upper = _mm512_set1_epi64(elements - 1);
 
-	while ((finished = _mm512_cmpneq_epi64_mask(_mm512_add_epi64(lower, one), upper)) != 0x00)
+	while ((not_finished = _mm512_cmpneq_epi64_mask(_mm512_add_epi64(lower, one), upper)) != 0x00)
 		{
 		__m512i middle = _mm512_srli_epi64(_mm512_add_epi64(upper, lower), 1);
 		__mmask8 results = _mm512_cmplt_epi64_mask(_mm512_i64gather_epi64(middle, array, sizeof(uint64_t)), key);
-		lower = _mm512_mask_blend_epi64 (finished & results, lower, middle);
-		upper = _mm512_mask_blend_epi64 (finished & ~results, upper, middle);
+		lower = _mm512_mask_blend_epi64 (not_finished & results, lower, middle);
+		upper = _mm512_mask_blend_epi64 (not_finished & ~results, upper, middle);
 		}
 	return upper;
 	}
@@ -32,43 +33,64 @@ __m512i avx_binary_search(uint64_t *array, int64_t elements, __m512i key)
 */
 int main(int argc, char *argv[])
 	{
+	constexpr int data_size = 1'000'000;
 	constexpr int seed = 17;
 	std::mt19937 random(seed);
-	std::uniform_int_distribution<int> distribution(0, guides.size() - 1);
+	std::uniform_int_distribution<int> distribution(0, data_size - 1);
 
+	std::vector<uint64_t> data;
+	std::vector<uint64_t> search_for;
 
+	for (size_t d = 0; d < 10; d++)
+		data.push_back(distribution(random));
+	sort(data.begin(), data.end());
 
-	std::cout << (1/2) << "\n";
+	for (size_t d = 0; d < 16'000'000; d++)
+		search_for.push_back(distribution(random));
 
-	uint64_t array[] = {3, 4, 5, 6, 7, 8, 9};
-	uint64_t search_for[] = {0, 3, 4, 5, 6, 7, 8, 9};
-	int n = sizeof(array) / sizeof(array[0]);
-	
-
-	for (uint64_t x = 0; x < sizeof(search_for) / sizeof(*search_for); x++)
+	std::cout << "64\n";
+	uint64_t odds = 0;
+	auto stopwatch_start = std::chrono::steady_clock::now();
+	for (uint64_t x = 0; x < search_for.size(); x++)
 		{
-		std::cout << "64\n";
-		auto found_at = binary_search(array, n, search_for[x]);
-		std::cout << search_for[x] << "***FOUND:" << found_at << "\n";
+		auto found_at = std::lower_bound(&data[0], &data[data.size() - 1], search_for[x]);
+		if ((found_at - &data[0]) & 0x01)
+			odds++;
+//		std::cout << search_for[x] << ':' << found_at - data.begin() << '\n';
 		}
+	auto stopwatch_end = std::chrono::steady_clock::now();
+	auto stopwatch_duration = std::chrono::duration_cast<std::chrono::microseconds>(stopwatch_end - stopwatch_start).count();
+	std::cout << "odds  : " << odds << '\n';
+	std::cout << "64-bit: " << stopwatch_duration / 1000000.001 << " seconds" << '\n';
 
+	std::cout << "512\n";
+	odds = 0;
+	stopwatch_start = std::chrono::steady_clock::now();
+	for (uint64_t x = 0; x < search_for.size(); x += 8)
 		{
-		std::cout << "512\n";
-		auto key = _mm512_load_epi64 (search_for);
-		auto found_at = avx_binary_search(array, n, key);
+		auto key = _mm512_load_epi64 (&search_for[x]);
+		auto found_at = avx_binary_search(&data[0], data.size(), key);
 
 		int64_t mem[8];
-
 		_mm512_store_epi64 (&mem, found_at);
-	
-		for (uint64_t x = 0; x < sizeof(search_for) / sizeof(*search_for); x++)
-			std::cout << search_for[x] << "***FOUND:" << mem[x] << "\n";
+		for (uint64_t result_pos = 0; result_pos < 8; result_pos++)
+			{
+			if (mem[result_pos] & 0x01)
+				odds++;
+//			std::cout << search_for[x + result_pos] << ':' << mem[result_pos] << '\n';
+			}
 		}
+	stopwatch_end = std::chrono::steady_clock::now();
+	stopwatch_duration = std::chrono::duration_cast<std::chrono::microseconds>(stopwatch_end - stopwatch_start).count();
+	std::cout << "odds  : " << odds << '\n';
+	std::cout << "512-bit:" << stopwatch_duration / 1000000.001 << " seconds" << '\n';
 
 	return 0;
 	}
 
 #ifdef NEVER
+				const uint64_t *found = std::lower_bound(index[index_key], index[index_key + 1], *current_key);
+
 	/*
 		The first occurance of key is at position upper if key is present in the array.
 		Else, if upper > elements then we've gone off the top of the array.
